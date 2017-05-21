e2b28031f4b5c046cac15c163abef4bcfa22021e
hunter.luo <hunter.luo@cbxsoftwear.com>
2017-05-21 16:54:24 +0800

    修复了监控页面显示不正常的bug

--- WebRoot/app/monitor/view/monitor_show.js
+++ WebRoot/app/monitor/view/monitor_show.js
@@ -1,78 +1,57 @@
-Ext
-		.define(
-				'MyApp.monitor.view.monitor_show',
-				{
-					extend : 'Ext.panel.Panel',
-					title : '监控中心',
-					alias : 'widget.MonitorCenter',
-					requires : [ 'Ext.app.PortalPanel' ],
-					layout : 'fit',
-					items : [ {
-						xtype : 'tabpanel',
-						tabPosition : 'left',
-						items : [
-								{
-									title : '区域一',
-									xtype : 'portalpanel',
-									autoScroll : true,
-									defaults : {
-										margin : '3 3 3 3'
-									},
-									items : [
-											{
-												defaults : {
-													margin : '5 0 0 0'
-												},
-												items : [
-														{
-															border : false,
-															closable : false,
-															collapsible : false,
-															height : 260,
-															html : '<video width="350" height="255" '
-																	+ 'src="resource/video/test.mp4"'
-																	+ ' autoplay="autoplay" loop="loop" muted="muted">'
-																	+ '</video>'
-														},
-														{
-															border : false,
-															closable : false,
-															collapsible : false,
-															height : 260
-														} ]
-											}, {
-												defaults : {
-													margin : '5 0 0 0'
-												},
-												items : [ {
-													border : false,
-													closable : false,
-													collapsible : false,
-													height : 260,
-												}, {
-													border : false,
-													closable : false,
-													collapsible : false,
-													height : 260,
-												} ]
-											}, {
-												defaults : {
-													margin : '5 0 0 0'
-												},
-												items : [ {
-													border : false,
-													closable : false,
-													collapsible : false,
-													height : 260,
-												}, {
-													border : false,
-													closable : false,
-													collapsible : false,
-													height : 260,
-												} ]
-											} ]
-								}, {
-									title : "test2",
-								} ]
-					} ]
-				});
+Ext.define('MyApp.monitor.view.monitor_show', {
+	extend : 'Ext.tab.Panel',
+	title : '监控中心',
+	alias : 'widget.MonitorCenter',
+	requires : [ 'Ext.app.PortalPanel' ],
+	layout : 'fit',
+	tabPosition : 'left',
+	defaults : {
+		margin : '3 3 3 3'
+	},
+	initComponent : function() {
+		var zoneStore = Ext.create("MyApp.berth.store.ZoneStore");
+		zoneStore.load({
+			callback : function(records) {
+				for ( var i = 0; i < records.length; i++) {
+					// 初始化区域
+					var zonePanel = Ext.create("Ext.app.PortalPanel", {
+						title : records[i].data.name,
+						id : records[i].data.id,
+						border:false,
+						listeners:{
+							beforerender:function( panel, eOpts ){
+								var monitorStore=Ext.create("MyApp.monitor.store.MonitorStore");
+								monitorStore.proxy.extraParams.zoneId = this.id;
+								monitorStore.load({callback:function(monitorRecords){
+									for ( var k = 0; k < 3; k++) {
+										var groupPanel = Ext.create("Ext.panel.Panel",{border:false});
+										for ( var m = 2*k; m < 2*k+2 ; m++) {
+											var url="";
+											if(monitorRecords[m]){
+												url=monitorRecords[m].data.url;
+											}
+											var monitorPanel = Ext.create("Ext.panel.Panel", {
+												closable : false,
+												collapsible : false,
+												height : 255,
+												html : '<video width="380" height="260" '
+													+ 'src="'+url+'"'
+													+ ' autoplay="autoplay" loop="loop" muted="muted">'
+													+ '</video>'
+											});
+											groupPanel.add(monitorPanel);
+										}
+										panel.add(groupPanel);
+									}
+								}});
+							}
+						}
+					});
+					this.add(zonePanel);
+				}
+			},
+			scope : this
+		})
+		this.callParent(arguments);
+	}
+});
